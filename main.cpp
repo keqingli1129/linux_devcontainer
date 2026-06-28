502256370ea0f38aa454b828d180bd0de24adcf7
Keqing Li <keqingli1129@gmail.com>
2026-06-28 21:55:35 +0000

    Add Eigen library integration and matrix multiplication example in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include "Chapter01/Lib/lib.h"
+#include <Eigen/Dense>
 
 using namespace std;
 int main() {
@@ -7,5 +8,16 @@ int main() {
     int32_t a = 10;
     auto b = 20.0F;
     printer(a);
+
+    // Eigen sample: 2x2 matrix multiplication
+    Eigen::Matrix2d mat;
+    mat << 1, 2,
+           3, 4;
+    Eigen::Vector2d vec(5, 6);
+    Eigen::Vector2d result = mat * vec;
+    cout << "Matrix:\n" << mat << endl;
+    cout << "Vector: " << vec.transpose() << endl;
+    cout << "Result: " << result.transpose() << endl;
+
     return 0;
 }
