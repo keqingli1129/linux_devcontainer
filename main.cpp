3d25e36893a1c494155fef3b421a68f1f10e515a
Keqing Li <keqingli1129@gmail.com>
2026-06-28 17:23:19 +0000

    Refactor project structure: remove Chapter01/Main files, add CMakeLists for Chapter01, and implement printer function in lib

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "Chapter01/Lib/lib.h"
+
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    int32_t a = 10;
+    printer(a);
+    return 0;
+}
